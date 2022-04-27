c6f4a046ab191e252c40782d1129a702f2ddf123
RLangley83 <103539220+RLangley83@users.noreply.github.com>
2022-04-27 15:25:50 -0400

    Add files via upload
    These are the classes used for the API assignment

---
+++ CityInfo.java
@@ -0,0 +1,6 @@
+public class CityInfo
+{
+   public boolean status;
+   public Data data;
+   
+}
---
+++ Data.java
@@ -0,0 +1,10 @@
+public class Data
+{
+   public String city;
+   public String state;
+   public String state_fullname;
+   public double latitude;
+   public double longitude;
+   public double population;
+
+}
